03fea35e9d50c0d0491b1b781eea011fcb86aa49
websterhf18 <websterh18@gmail.com>
2021-08-20 18:58:45 -0500

    Update

--- app/Http/Controllers/VehiculosController.php
+++ app/Http/Controllers/VehiculosController.php
@@ -93,7 +93,7 @@ class VehiculosController extends Controller
                 'vehicles.id', 'vehicles.tipo_moto', 'vehicles.title', 'vehicles.precio','vehicles.condicion',
                 'vehicles.ano', 'vehicles.kilometraje', 'UC.nombre AS labelCiudad',
                 'I.nombre AS nameImage', 'I.extension', 'I.new_image', 'M.nombre AS modelo', 'MA.nombre AS marca', 
-                'MO.nombre AS combustible', 'TA.nombre AS transmision', 'TM.nombre AS tipoMotoLabel'
+                'MO.nombre AS combustible', 'TA.nombre AS transmision'
             )
             ->join('imagenes AS I', 'I.id_vehicle', \DB::raw('vehicles.id AND I.order = 1'))
             ->join('ubicacion_ciudades AS UC', 'UC.id', 'vehicles.ciudad_id')
@@ -101,7 +101,6 @@ class VehiculosController extends Controller
             ->join('marcas AS MA', 'MA.id', 'M.marca_id')
             ->join('combustibles AS MO', 'MO.id', 'vehicles.combustible')
             ->join('transmisiones AS TA', 'TA.id', 'vehicles.transmision')
-            ->join('tipo_moto AS TM', 'TM.id', 'vehicles.tipo_moto')
             ->where('vehicles.activo', 1);
         //Tag search Filter
         if( $filtros['q'] ){
@@ -118,6 +117,9 @@ class VehiculosController extends Controller
         }
         //Cateoria Filter
         if ($filtros['categoria']) {
+            if($filtros['categoria'] === 'motos'){
+                $result->select('TM.nombre AS tipoMotoLabel')->join('tipo_moto AS TM', 'TM.id', 'vehicles.tipo_moto')
+            }
             $result->where('vehicles.tipo_vehiculo', $this->parse_slug_id($filtros['categoria']));
         } else {
             $result->where('vehicles.tipo_vehiculo', 1);
@@ -197,10 +199,12 @@ class VehiculosController extends Controller
             'anios' => $filteredMarcas->countBy('ano'),
             'caja' => $filteredMarcas->countBy('transmision'),
             'combustible' => $filteredMarcas->countBy('combustible'),
-            'tipo' => $filteredMarcas->countBy('tipoMotoLabel'),
+            'tipo' => array(),
             'ubicacion' => $filteredMarcas->countBy('labelCiudad')
         );
-
+        if($filtros['categoria'] === 'motos'){
+            $contadores['tipo'] = $filteredMarcas->countBy('tipoMotoLabel');
+        }
         $response = [
             'page' => $filtros['page'],
             'total_records' => $total_records,
